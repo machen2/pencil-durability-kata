4982965cf6f7c661b5e7dfb1ce274fd68ede3959
machen2 <victoriamachen@gmail.com>
2017-09-28 12:32:05 -0500

    refactored the tests for cli class call and welcome_user methods

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,19 +1,20 @@
 require "spec_helper"
 
 RSpec.describe PencilDurability::Cli do 
+    let(:cli) { PencilDurability::Cli.new }
+    
     it "is defined as a class" do 
         expect(PencilDurability::Cli).to be_a(Class)
     end
 
-    describe PencilDurability::Cli, '#call' do
-        cli = PencilDurability::Cli.new
-        it "welcomes the user" do 
-            expect{cli.call}.to output("Welcome!\n").to_stdout
+    describe '#call' do
+        it 'calls the welcome_user method' do 
+            expect(cli).to receive(:welcome_user)
+            cli.call
         end
     end
 
-    describe PencilDurability::Cli, '#welcome_user' do 
-        cli = PencilDurability::Cli.new
+    describe '#welcome_user' do 
         it "welcomes the user with a message" do 
             expect{cli.welcome_user}.to output("Welcome!\n").to_stdout
         end
