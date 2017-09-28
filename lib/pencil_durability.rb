a4442cf7c8b3819e8ad15946621bd49a91b1ab62
machen2 <victoriamachen@gmail.com>
2017-09-28 13:26:21 -0500

    added test and code for a paper class

--- lib/pencil_durability.rb
+++ lib/pencil_durability.rb
@@ -1,2 +1,3 @@
 require_relative "pencil_durability/version"
 require_relative "pencil_durability/cli"
+require_relative "pencil_durability/paper"
---
+++ spec/paper_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper"
+
+RSpec.describe PencilDurability::Paper do 
+    it "is defined as a class" do 
+        expect(PencilDurability::Paper).to be_a(Class)
+    end
+end
