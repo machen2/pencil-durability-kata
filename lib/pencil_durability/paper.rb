215015e7136a263c068e050d053f12bbbe86da4e
machen2 <victoriamachen@gmail.com>
2017-09-28 14:13:49 -0500

    added test for ability to create an instance of Paper

--- lib/pencil_durability/paper.rb
+++ lib/pencil_durability/paper.rb
@@ -1,3 +1,3 @@
 class PencilDurability::Paper
-
+    
 end
--- spec/paper_spec.rb
+++ spec/paper_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe PencilDurability::Paper do
     it "is defined as a class" do 
         expect(PencilDurability::Paper).to be_a(Class)
     end
+
+    let(:paper) { PencilDurability::Paper.new }
+    it "must able to create an instance of Paper" do 
+        expect(paper).to be_a(PencilDurability::Paper)
+    end
 end
