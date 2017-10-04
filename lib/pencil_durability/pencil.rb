926b30d256badcf79c5f52cde13f43719dadfee3
machen2 <victoriamachen@gmail.com>
2017-10-04 17:24:59 -0500

    added test for initializing a new pencil object

--- lib/pencil_durability/pencil.rb
+++ lib/pencil_durability/pencil.rb
@@ -1,3 +1,4 @@
 class PencilDurability::Pencil
-
+    def initialize
+    end
 end
--- spec/pencil_spec.rb
+++ spec/pencil_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe PencilDurability::Pencil do
     it "is defined as a class" do 
         expect(PencilDurability::Pencil).to be_a(Class)
     end
+
+    describe "#new" do 
+        it "initializes a new instance of pencil" do 
+            expect{PencilDurability::Pencil.new}.to_not raise_error
+        end
+    end 
+
 end
